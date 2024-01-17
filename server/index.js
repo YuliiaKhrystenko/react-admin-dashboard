2baa2aab849f9ea14bb2676612e7ddd8bd058f3e
Yuliia <yu6785963@gmail.com>
2024-01-17 09:53:36 +0200

    add overview, daily, monthly page

--- server/index.js
+++ server/index.js
@@ -14,8 +14,9 @@ import salesRoutes from "./routes/sales.js"
 import User from './models/User.js';
 import Product from './models/Product.js';
 import ProductStat from './models/ProductStat.js';
-// import Transaction from './models/Transaction.js';
-import { dataUser, dataProduct, dataProductStat, dataTransaction } from "./data/index.js"
+import Transaction from './models/Transaction.js';
+import OverallStat from "./models/OverallStat.js"
+import { dataUser, dataProduct, dataProductStat, dataTransaction, dataOverallStat } from "./data/index.js"
 
 // CONFIGURATION
 dotenv.config()
@@ -43,6 +44,7 @@ mongoose.connect(process.env.MONGO_URL, {
 }).then(() => {
   app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
 
+  // OverallStat.insertMany(dataOverallStat)
   // User.insertMany(dataUser)
   // Product.insertMany(dataProduct)
   // ProductStat.insertMany(dataProductStat)
